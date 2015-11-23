89956a9b16ccd3184ab5004e644abfbaa57be274
rkenavey <ryan@olo.com>
2015-11-23 13:24:08 -0500

    Another hello

--- app/SampleConsoleApp/SampleConsoleApp/Program.cs
+++ app/SampleConsoleApp/SampleConsoleApp/Program.cs
@@ -11,6 +11,7 @@ namespace SampleConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
+            Console.WriteLine("Hello world again!!");
             Console.Read();
         }
     }
