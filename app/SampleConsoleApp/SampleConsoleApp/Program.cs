e0d2e5ee888ae32b2a3e234236f9a2044bdc84dd
rkenavey <ryan@olo.com>
2015-11-23 13:23:37 -0500

    Hello and wait for input.

--- app/SampleConsoleApp/SampleConsoleApp/Program.cs
+++ app/SampleConsoleApp/SampleConsoleApp/Program.cs
@@ -10,6 +10,8 @@ namespace SampleConsoleApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello world!");
+            Console.Read();
         }
     }
 }
